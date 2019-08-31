89ff4d737c1f8ab037cb47a5a9837b6857ea5057
Corey Auger <coreyauger@gmail.com>
2019-08-30 22:01:25 -0700

    0.0.8

--- typebus/src/main/scala/io/surfkit/typebus/entity/EntityDb.scala
+++ typebus/src/main/scala/io/surfkit/typebus/entity/EntityDb.scala
@@ -2,8 +2,15 @@ package io.surfkit.typebus.entity
 
 import io.surfkit.typebus.{ByteStreamReader, ByteStreamWriter}
 
+import scala.concurrent.Future
 import scala.reflect.ClassTag
 
 abstract class StatefulEntity[S : ClassTag : ByteStreamReader : ByteStreamWriter] {
   // TODO: ..
+}
+
+trait CQRSDatabase[S]{
+  def getState(id: String): Future[S]
+  // TODO compensating action
+  //def modifyState(id: String, state: S):
 }
